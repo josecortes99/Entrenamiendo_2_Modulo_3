34f92feaab3a098ad1e28528ea874deebc9a9b78
Jose Cortes <j.c.e555@hotmail.com>
2025-06-26 10:17:09 -0500

    entrenamiento Terminado

---
+++ gestion_datos.js
@@ -0,0 +1,45 @@
+console.log("Gestion de Datos con Objetos, Maps y Sets.");
+
+const productos = {
+    1: {id: 1, nombre: "laptop", precio: 1500},
+    2: {id: 2, nombre: "mouse", precio: 25},
+    3: {id: 3, nombre: "teclado", precio: 50},
+    4: {id: 4, nombre: "teclado", precio: 50},
+    5: {id: 5, nombre: "savia", precio: 50}
+};
+
+console.log("Objeto Productos", productos);
+
+//Creacion de Set 
+const setProductos = new Set(Object.values(productos).map(producto => producto.nombre));
+console.log("Set Productos Unicos", setProductos);
+
+//Creacion Map
+const mapProductos = new Map([
+    ["Electronica", "laptop"],
+    ["Accesorios", "mouse"],
+    ["Accesorios", "teclado"],
+    ["botanica", "savia"]
+]);
+console.log("Map de Productos y Ctegorias:", mapProductos);
+
+//Recorrer el objeto productos
+for (const id in productos) {
+    console.log(`Producto ID: ${id}, Detalles:`, productos[id]);
+}
+
+//Recorrer el Set
+for (const producto of setProductos) {
+    console.log("Producto único:", producto);
+}
+
+//Recorrer Map
+mapProductos.forEach((producto, categoria) => {
+    console.log(`categoria: ${categoria}, Producto: ${producto}`);
+});
+
+//Validaciones
+console.log("Pruebas completas de gestion de datos");
+console.log("Lista de productos (Objeto):", productos);
+console.log("Lista de productos unicos (Set):", setProductos);
+console.log("Categorias y productos (Map):", mapProductos);
